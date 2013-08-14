2f671b1c4f1e917b88ef65e718651c60058e0c1a
RyanTM (Pololu) <dev-ryantm@pololu.com>
2013-08-14 08:40:07 -0700

    try 3

--- lib/life.rb
+++ lib/life.rb
@@ -1,2 +1,30 @@
 class Life
+  def Life.evolve(world)
+    world.collect.with_index do |row, y|
+      row.collect.with_index do |cell, x|
+        case neighbors(world,x,y)
+        when 0,1,4..8
+          0
+        when 2
+          cell
+        when 3
+          1
+        end
+      end
+    end
+  end
+
+  def Life.neighbors(world,x,y)
+    [ [-1,-1], [0,-1], [1, -1],
+      [-1, 0],         [1,  0],
+      [-1, 1], [0, 1], [1,  1]
+    ].inject(0) do |sum, offset|
+      x_offset = offset[0]
+      y_offset = offset[1]
+      y_offset = 0 if y+y_offset >= world.size
+      row = world[y+y_offset]
+      x_offset = 0 if x+x_offset >= row.size
+      sum + row[x+x_offset]
+    end
+  end
 end
--- spec/life_spec.rb
+++ spec/life_spec.rb
@@ -1,4 +1,58 @@
 require 'spec_helper'
 
 describe Life do
+  describe "::neighbors" do
+    context "there is one cell in the world" do
+      let(:world) do
+        [ [0,0,0],
+          [0,1,0],
+          [0,0,0]]
+      end
+      it "should return the neighbor count" do
+        expect(Life::neighbors(world,1,1)).to eq(0)
+        expect(Life::neighbors(world,0,0)).to eq(1)
+        expect(Life::neighbors(world,1,0)).to eq(1)
+        expect(Life::neighbors(world,2,0)).to eq(1)
+        expect(Life::neighbors(world,1,2)).to eq(1)
+        expect(Life::neighbors(world,2,2)).to eq(1)
+        expect(Life::neighbors(world,1,2)).to eq(1)
+        expect(Life::neighbors(world,0,2)).to eq(1)
+        expect(Life::neighbors(world,0,1)).to eq(1)
+      end
+    end
+    context "there are two cells in the world" do
+      let(:world) do
+        [ [0,0,0],
+          [0,1,1],
+          [0,0,0]]
+      end
+      it "should return the neighbor count" do
+        expect(Life::neighbors(world,1,1)).to eq(1)
+        expect(Life::neighbors(world,1,0)).to eq(2)
+        expect(Life::neighbors(world,0,1)).to eq(2)
+      end
+    end
+  end
+  describe '::evolve' do
+    context 'blinker' do
+      let(:world1) {
+        [ [0,0,0,0,0],
+          [0,0,1,0,0],
+          [0,0,1,0,0],
+          [0,0,1,0,0],
+          [0,0,0,0,0] ]
+      }
+      let(:world2) {
+        [ [0,0,0,0,0],
+          [0,0,0,0,0],
+          [0,1,1,1,0],
+          [0,0,0,0,0],
+          [0,0,0,0,0] ]
+      }
+      it 'should blink' do
+        expect(Life::evolve(world1)).to eq_board(world2)
+        expect(Life::evolve(world2)).to eq_board(world1)
+      end
+    end
+  end
 end
